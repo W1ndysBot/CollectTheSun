20c0e2a9968578ceae3549615226fe680cfbaef6
W1ndys <w1ndys@outlook.com>
2024-08-30 23:56:17 +0800

    收集阳光！

--- main.py
+++ main.py
@@ -192,11 +192,16 @@ def update_rain(group_id, user_id, rain_count):
 
 # 菜单
 async def sun_menu(websocket, group_id, message_id):
-    content = f"""[CQ:reply,id={message_id}]收集阳光指令列表：
+    content = f"""[CQ:reply,id={message_id}]来为 24 级新生收集阳光吧！每次收集阳光或雨水，都会随机增加 0-50 阳光或雨水。阳光减去雨水为有效阳光。本玩法为限时娱乐玩法，适度娱乐，切勿当真。数据全服互通，你的数据将会基于所有群聊的数据结算，切勿频繁刷分喔，否则会被关进小黑屋，玩法将于 9 月 14 日 0 点军训结束准时关服。排行榜也是全服实时结算，偷偷告诉你，这是 W1ndys 第一次写全服互通的玩法，快来看看他有没有写出愚蠢的 bug。
+特殊玩法：奇遇事件，群里的每一条消息都有 1% 的概率触发奇遇事件并收集 0-20 阳光或雨水（作者脑子已经炸了）。
+
+
+收集阳光指令列表：
 收集阳光：收集阳光 或 sun
-呼风唤雨：呼风唤雨 或 rain
-查看信息：查看信息 或 info
-阳光排行榜：阳光排行榜 或 rank"""
+收集雨水：收集雨水 或 rain
+查看信息：查看信息 或 suninfo
+阳光排行榜：阳光排行榜 或 sunrank
+想加新玩法或建议，联系https://blog.w1ndys.top/html/QQ.html"""
     await send_group_msg(websocket, group_id, content)
 
 
@@ -212,19 +217,19 @@ async def collect_sun(websocket, group_id, user_id, message_id):
         )
         return
 
-    sun_count = random.randint(1, 50)
+    sun_count = random.randint(0, 50)
     update_sun(group_id, user_id, sun_count)
     await send_group_msg(
         websocket,
         group_id,
-        f"[CQ:reply,id={message_id}]本次收集了{sun_count}个阳光，祝24级新生军训愉快！\n"
+        f"[CQ:reply,id={message_id}]本次收集了{sun_count}颗阳光，祝24级新生军训愉快！\n"
         f"——————————\n"
-        f"发送“info”查看信息，发送“rank”查看阳光排行榜，发送“sunmenu”查看所有命令",
+        f"发送“suninfo”查看信息，发送“sunrank”查看阳光排行榜，发送“sunmenu”查看所有命令",
     )
 
 
-# 呼风唤雨，随机减少阳光
-async def call_rain(websocket, group_id, user_id, message_id):
+# 随机收集雨水
+async def collect_rain(websocket, group_id, user_id, message_id):
 
     # 检测日期
     if datetime.datetime.now() > datetime.datetime(2024, 9, 14):
@@ -240,27 +245,138 @@ async def call_rain(websocket, group_id, user_id, message_id):
     await send_group_msg(
         websocket,
         group_id,
-        f"[CQ:reply,id={message_id}]本次呼风唤雨了{rain_count}个雨水，祝24级新生军训愉快！\n"
+        f"[CQ:reply,id={message_id}]本次收集了{rain_count}滴雨水，祝24级新生军训愉快！\n"
         f"——————————\n"
         f"发送“suninfo”查看信息，发送“sunrank”查看阳光排行榜，发送“sunmenu”查看所有命令",
     )
+
+
+# 获取本群有效阳光前三的用户
+def get_top_three_sun(group_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT user_id, (sun_count - rain_count) as effective_sun FROM collect_the_sun WHERE group_id = ? ORDER BY effective_sun DESC LIMIT 3",
+        (group_id,),
+    )
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+
+# 获取全服有效阳光前三的用户
+def get_top_three_sun_all():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT user_id, (sun_count - rain_count) as effective_sun FROM collect_the_sun ORDER BY effective_sun DESC LIMIT 3",
+    )
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+
+# 获取总阳光最多的前三个群
+def get_top_three_group_sun():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT group_id, SUM(sun_count - rain_count) as effective_sun FROM collect_the_sun GROUP BY group_id ORDER BY effective_sun DESC LIMIT 3",
+    )
+    result = cursor.fetchall()
+    conn.close()
+    return result
 
 
 # 查看信息
 async def check_info(websocket, group_id, user_id, message_id):
     content = (
         f"[CQ:reply,id={message_id}]"
-        f"你在本群收集阳光：{load_user_sun(group_id, user_id)}个，收集雨水：{load_user_rain(group_id, user_id)}个\n"
-        f"你共收集阳光：{load_user_all_sun(user_id)}个，收集雨水：{load_user_all_rain(user_id)}个\n"
-        f"本群总收集阳光：{load_group_all_sun(group_id)}个，收集雨水：{load_group_all_rain(group_id)}个\n"
-        f"全服总收集阳光：{load_all_sun()}个，收集雨水：{load_all_rain()}个"
+        f"你在本群收集阳光：{load_user_sun(group_id, user_id)}个，收集雨水：{load_user_rain(group_id, user_id)}个，有效阳光：{load_user_sun(group_id, user_id) - load_user_rain(group_id, user_id)}个\n"
+        f"你共收集阳光：{load_user_all_sun(user_id)}个，收集雨水：{load_user_all_rain(user_id)}个，有效阳光：{load_user_all_sun(user_id) - load_user_all_rain(user_id)}个\n"
+        f"本群总收集阳光：{load_group_all_sun(group_id)}个，收集雨水：{load_group_all_rain(group_id)}个，有效阳光：{load_group_all_sun(group_id) - load_group_all_rain(group_id)}个\n"
+        f"全服总收集阳光：{load_all_sun()}个，收集雨水：{load_all_rain()}个，有效阳光：{load_all_sun() - load_all_rain()}个"
     )
     await send_group_msg(websocket, group_id, content)
 
 
 # 阳光排行榜
 async def sun_rank(websocket, group_id, user_id, message_id):
-    pass
+    top_three_sun = get_top_three_sun(group_id)
+    top_three_sun_all = get_top_three_sun_all()
+    top_three_group_sun = get_top_three_group_sun()
+    content = f"[CQ:reply,id={message_id}]"
+    content += f"本群有效阳光前三的用户：\n"
+    for rank, (user_id, sun_count) in enumerate(top_three_sun, 1):
+        content += f"{rank}. <{user_id}>: {sun_count}颗阳光\n"
+    content += f"\n全服有效阳光前三的用户：\n"
+    for rank, (user_id, sun_count) in enumerate(top_three_sun_all, 1):
+        content += f"{rank}. <{user_id}>: {sun_count}颗阳光\n"
+    content += f"\n全服有效阳光前三的群：\n"
+    for rank, (group_id, sun_count) in enumerate(top_three_group_sun, 1):
+        content += f"{rank}. <{group_id}>: {sun_count}颗阳光\n"
+    await send_group_msg(websocket, group_id, content)
+
+
+# 每句话随机添加阳光
+async def random_add(websocket, group_id, user_id, message_id):
+    events = [
+        "你在格物楼捡到了",
+        "你在吱吱楼找到了",
+        "你在文渊楼发现了",
+        "你在操场上遇到了",
+        "你在军训操场找到了",
+        "你在综合楼里发现了",
+        "你在宿舍楼捡到了",
+        "你在图书馆里找到了",
+        "你在体育馆里发现了",
+        "你在食堂里找到了",
+        "你在符文大陆找到了",
+        "你在王者峡谷找到了",
+        "你在蛋仔岛找到了",
+        "你在平安京找到了",
+        "你在提瓦特大陆找到了",
+        "你在黑神话悟空找到了",
+        "你在做俯卧撑时找到了",
+        "你在罚站时找到了",
+        "你在整理床铺时找到了",
+        "你在瓦罗兰特找到了",
+        "你在木叶村找到了",
+        "你在西联找到了",
+        "你在文史楼找到了",
+        "你在绘素楼找到了",
+        "你在背论语的时候找到了",
+        "你在背单词的时候找到了",
+        "你在背公式的时候找到了",
+        "你在写代码找到了",
+        "你在暴打W1ndys的时候找到了",
+        "你在暴打W1ndys的时候找到了",
+        "你在暴打W1ndys的时候找到了",
+        "你在暴打W1ndys的时候找到了",
+        "你在暴打W1ndys的时候找到了",
+        "你在暴打W1ndys的时候找到了",
+    ]
+
+    if random.random() < 0.05:
+        sun_count = random.randint(0, 20)
+        event = random.choice(events)
+        if random.random() < 0.5:  # 百分之五十的概率收集阳光
+            update_sun(group_id, user_id, sun_count)
+            await send_group_msg(
+                websocket,
+                group_id,
+                f"[CQ:reply,id={message_id}]{event}{sun_count}颗阳光，祝24级新生军训愉快！",
+            )
+            logging.info(f"触发奇遇事件，{user_id}在{group_id}添加{sun_count}颗阳光")
+
+        else:  # 百分之五十的概率收集雨水
+            update_rain(group_id, user_id, sun_count)
+            await send_group_msg(
+                websocket,
+                group_id,
+                f"[CQ:reply,id={message_id}]{event}{sun_count}滴雨水，祝24级新生军训愉快！",
+            )
+            logging.info(f"触发奇遇事件，{user_id}在{group_id}添加{sun_count}滴雨水")
 
 
 # 群消息处理函数
@@ -291,8 +407,8 @@ async def handle_CollectTheSun_group_message(websocket, msg):
             await collect_sun(websocket, group_id, user_id, message_id)
             return
 
-        if raw_message == "呼风唤雨" or raw_message == "rain":
-            await call_rain(websocket, group_id, user_id, message_id)
+        if raw_message == "收集雨水" or raw_message == "rain":
+            await collect_rain(websocket, group_id, user_id, message_id)
             return
 
         if raw_message == "查看信息" or raw_message == "suninfo":
@@ -302,6 +418,10 @@ async def handle_CollectTheSun_group_message(websocket, msg):
         if raw_message == "阳光排行榜" or raw_message == "sunrank":
             await sun_rank(websocket, group_id, user_id, message_id)
             return
+
+        # 如果不是上述命令，进入每句话随机添加函数
+        await random_add(websocket, group_id, user_id, message_id)
+        return
 
     except Exception as e:
         logging.error(f"处理CollectTheSun群消息失败: {e}")
