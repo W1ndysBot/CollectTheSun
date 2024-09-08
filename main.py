bf8d5bb11bb69990e37d738229446847cff6641c
W1ndys <w1ndys@outlook.com>
2024-09-09 00:15:33 +0800

    注释日志

--- main.py
+++ main.py
@@ -230,7 +230,7 @@ def update_cd(group_id, user_id):
                 "UPDATE collect_the_sun SET time = ? WHERE group_id = ? AND user_id = ?",
                 (time, group_id, user_id),
             )
-    logging.info(f"更新用户{user_id}在群{group_id}的CD:{time}")
+    # logging.info(f"更新用户{user_id}在群{group_id}的CD:{time}")
     conn.commit()
     conn.close()
 
@@ -260,7 +260,7 @@ def update_sun(group_id, user_id, sun_count):
                 "UPDATE collect_the_sun SET sun_count = ? WHERE group_id = ? AND user_id = ?",
                 (total_sun_count, group_id, user_id),
             )
-    logging.info(f"更新用户{user_id}在群{group_id}的阳光:{total_sun_count}")
+    # logging.info(f"更新用户{user_id}在群{group_id}的阳光:{total_sun_count}")
     conn.commit()
     conn.close()
     return True
@@ -512,7 +512,7 @@ async def check_user_info(
 
 # 阳光排行榜
 async def sun_rank(websocket, group_id, message_id):
-    logging.debug(f"群号:{group_id}")
+    # logging.debug(f"群号:{group_id}")
     top_three_sun = get_top_three_sun(group_id)
     top_three_sun_all = get_top_three_sun_all()
     top_three_group_sun = get_top_three_group_sun()
@@ -520,15 +520,15 @@ async def sun_rank(websocket, group_id, message_id):
     content += f"本群有效阳光前三的用户:\n"
     for rank, (user_id, sun_count) in enumerate(top_three_sun, 1):
         content += f"{rank}. <{user_id}>: {sun_count}阳光\n"
-    logging.debug(f"群号:{group_id}")
+    # logging.debug(f"群号:{group_id}")
     content += f"\n全服有效阳光前三的用户:\n"
     for rank, (user_id, sun_count) in enumerate(top_three_sun_all, 1):
         content += f"{rank}. <{user_id}>: {sun_count}阳光\n"
-    logging.debug(f"群号:{group_id}")
+    # logging.debug(f"群号:{group_id}")
     content += f"\n全服有效阳光前三的群:\n"
     for rank, (group_id_in_db, sun_count) in enumerate(top_three_group_sun, 1):
         content += f"{rank}. <{group_id_in_db}>: {sun_count}阳光\n"
-    logging.debug(f"群号:{group_id}")
+    # logging.debug(f"群号:{group_id}")
     await send_group_msg(websocket, group_id, content)
 
 
